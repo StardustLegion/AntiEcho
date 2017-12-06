2abcc5b50e0c62311857002977356026fb1ee3b2
jiminykbob <jikang92@hotmail.com>
2017-12-06 12:34:19 -0800

    fixed clientside response and populated newsfeed

--- client/src/components/NewsFeedItem.js
+++ client/src/components/NewsFeedItem.js
@@ -1,11 +1,50 @@
 import React from 'react';
 
-const NewsFeedItem = () => {
+const NewsFeedItem = props => {
   return (
     <div>
       NEWSFEEDITEM
+      <a href={props.url}>{props.title}</a>
+      {props.description}
     </div>
   );
 };
 
 export default NewsFeedItem;
+
+// import React from 'react';
+// import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+// import FlatButton from 'material-ui/FlatButton';
+//
+// const CardExampleExpandable = () => (
+//   <Card>
+//     <CardHeader
+//       title="Without Avatar"
+//       subtitle="Subtitle"
+//       actAsExpander={true}
+//       showExpandableButton={true}
+//     />
+//     <CardActions>
+//       <FlatButton label="Action1" />
+//       <FlatButton label="Action2" />
+//     </CardActions>
+//     <CardText expandable={true}>
+//       Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+//       Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+//       Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+//       Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+//     </CardText>
+//   </Card>
+// );
+
+// export default CardExampleExpandable;
+
+{/* <NewsFeedItem
+  key={i}
+  id={article._id}
+  author={article.author}
+  title={article.title}
+  description={article.description}
+  url={article.url}
+  urlToImage={article.urlToImage}
+/> */}
--- client/src/store.js
+++ client/src/store.js
@@ -3,22 +3,12 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers/index';
 import thunkMiddleware from 'redux-thunk';
 
-const loggerMiddleware = createLogger();
-
 const store = createStore(
-<<<<<<< HEAD
-    reducers,
-    composeWithDevTools(),
-    applyMiddleware(
-        thunkMiddleware,
-    ),
-=======
   reducers,
   composeWithDevTools(),
   applyMiddleware(
     thunkMiddleware
   ),
->>>>>>> 79e67eeeb24e9811098a0783b3d97fede257ce26
 
 );
 
