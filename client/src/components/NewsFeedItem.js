16315eab5e87aad499513278c189d66a15689852
jiminykbob <jikang92@hotmail.com>
2017-12-06 16:49:12 -0800

    changed cards to display image on load

--- client/src/components/NewsFeedItem.js
+++ client/src/components/NewsFeedItem.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
 const NewsFeedItem = props => {
   return (
     <div id="card">
       <Card>
+    <CardHeader
+      title={props.title}
+      subtitle={props.source}
+      avatar={props.urlToImage}
+    />
+    <CardMedia
+      overlay={<CardTitle title={props.title} subtitle={props.source} />}
+    >
+      <img src={props.urlToImage} alt="" />
+    </CardMedia>
+    <CardTitle title={props.source} subtitle={props.author} />
+    <CardText>
+      <a href={props.url}>{props.description}</a>
+    </CardText>
+  </Card>
+      {/* <Card>
         <CardHeader
           title={props.title}
           subtitle={props.source}
@@ -18,7 +34,7 @@ const NewsFeedItem = props => {
           <a href={props.url}>{props.description}</a>
           <img src={props.urlToImage} />
         </CardText>
-      </Card>
+      </Card> */}
     </div>
   );
 };
